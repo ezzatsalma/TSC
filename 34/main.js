9b38eb0bec60274380ba44faf9386d34f5e47a4f
ezzatsalma <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 16:34:23 +0500

    Add files via upload

---
+++ 34/main.js
@@ -0,0 +1,6 @@
+var pizzas = ['bbq', 'cheese', 'pepperoni'];
+for (var _i = 0, pizzas_1 = pizzas; _i < pizzas_1.length; _i++) {
+    var pizza = pizzas_1[_i];
+    console.log("".concat(pizza, " is my favourite"));
+}
+console.log("i love pizza");
---
+++ 34/main.ts
@@ -0,0 +1,5 @@
+let pizzas=['bbq','cheese','pepperoni']
+for(let pizza of pizzas){
+console.log(`${pizza} is my favourite`)
+}
+console.log("i love pizza")
