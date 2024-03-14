0931acc31f6704522405289b49d39e71f498b613
SALMA EZZAT <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 17:12:50 +0500

    Add files via upload

---
+++ 45/main.js
@@ -0,0 +1,6 @@
+function createcar(manufacture, name, color, feature) {
+    console.log("manufacturer:".concat(manufacture), "name:".concat(name), "color:".concat(color), "feature:".concat(feature));
+}
+createcar("BMW", "Mercedees Benz", "White");
+createcar("KIA", "SPORTAGE");
+createcar("HYUNDAI", "TUSCON", "", "NAVIGATION");
---
+++ 45/main.ts
@@ -0,0 +1,6 @@
+function createcar(manufacture,name,color?,feature?){
+    console.log(`manufacturer:${manufacture}`,`name:${name}`,`color:${color}`,`feature:${feature}`)
+}
+createcar("BMW","Mercedees Benz","White")
+createcar("KIA","SPORTAGE")
+createcar("HYUNDAI","TUSCON","","NAVIGATION")
