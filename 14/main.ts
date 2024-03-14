358a83f7d9e6574f7bd48c676781e751bc2213b2
ezzatsalma <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 16:17:16 +0500

    Add files via upload

---
+++ 14/main.ts
@@ -0,0 +1,6 @@
+let names = ['Ammar','salma','Hafsa','Ammara']
+let message=" 'you are invited to dinner'"
+console.log(names[0]+message)
+console.log(names[1] + message)
+console.log(names[2]+message)
+console.log(names[3] + message)
