5c31681ca43b23b8e93e4f010e1275ab520e6e4a
ezzatsalma <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 16:07:45 +0500

    Add files via upload

---
+++ 05/variiquote.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = "Sir Francis Bacon";
+let saying = "Knowledge is power";
+console.log(famous_person, "once said", saying);
---
+++ 05/variiquote.ts
@@ -0,0 +1,3 @@
+let famous_person="Sir Francis Bacon";
+let saying:string="Knowledge is power";
+console.log(famous_person"once said" saying);
