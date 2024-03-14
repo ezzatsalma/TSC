38bf38ba782ef04bae554faef9e65a488fdd9ed6
SALMA EZZAT <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 17:08:31 +0500

    Add files via upload

---
+++ 39/main.js
@@ -0,0 +1,6 @@
+function country_city(city, country) {
+    console.log("".concat(city, ",").concat(country));
+}
+country_city('Lahore', 'Pakistan');
+country_city('seoul', 'Korea');
+country_city('London', 'England');
---
+++ 39/main.ts
@@ -0,0 +1,7 @@
+function country_city(city:string,country:string): void{
+    console.log(`${city},${country}`)
+
+}
+country_city('Lahore','Pakistan')
+country_city('seoul','Korea')
+country_city('London','England')
