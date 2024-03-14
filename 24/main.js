65b456052b5fcd0d2c194382b5c2d375ae87b1d7
ezzatsalma <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 16:25:38 +0500

    Add files via upload

---
+++ 24/main.js
@@ -0,0 +1,19 @@
+//inequalities
+var str1 = "ahmed";
+var str2 = "afsha";
+console.log(str1 == str2);
+console.log(str1 != str2);
+//lowercase
+var a = 'HELLOWORLD';
+console.log(a.toLowerCase() == 'helloworld');
+//numerical testing
+var b = 1, c = 2, d = 3;
+console.log(c == b);
+console.log(c != b);
+console.log(c < b);
+console.log(c > b);
+console.log(c <= b);
+console.log(c >= b);
+// and and or
+console.log(c > b && c > d);
+console.log(c > b || c > d);
