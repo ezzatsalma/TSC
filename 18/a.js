3dacab6adf66a03a6365c5a830ca625b9202f498
ezzatsalma <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 16:20:31 +0500

    Add files via upload

---
+++ 18/a.js
@@ -0,0 +1,13 @@
+"use strict";
+let a = ['a', 'b', 'e', 'd', 'c'];
+console.log(a);
+{
+    console.log(a.slice().sort());
+}
+console.log(a);
+{
+    console.log(a.slice().reverse());
+}
+console.log(a);
+console.log(a.slice().sort());
+console.log(a.slice().reverse());
