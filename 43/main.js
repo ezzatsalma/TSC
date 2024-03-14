6d123e14d2505f490529dd5075fb7d57681cdf4c
SALMA EZZAT <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 17:11:38 +0500

    Add files via upload

---
+++ 43/main.js
@@ -0,0 +1,7 @@
+var magiciansname = ['David Copperfield', 'Shin Lim', 'David Blaine'];
+function show_magician(magiciansname) {
+    for (var i = 0; i < magiciansname.length; i++) {
+        console.log(magiciansname[i]);
+    }
+}
+show_magician(magiciansname);
