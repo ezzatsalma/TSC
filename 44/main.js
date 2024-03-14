32f712d1d8b3f23f7a80af5d03240ce550767d5b
SALMA EZZAT <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 17:12:16 +0500

    Add files via upload

---
+++ 44/main.js
@@ -0,0 +1,7 @@
+function makeSandwitch(item1, item2, item3) {
+    console.log("your sandwitch's menu include ".concat(item1, ",").concat(item2, ",").concat(item3));
+    console.log('sandwitch complete');
+}
+makeSandwitch('mayo', 'onions', 'eggs');
+makeSandwitch('ketchup', 'mushroom', 'tomato');
+makeSandwitch('bbq sauce', 'onions', 'mushrooms');
---
+++ 44/main.ts
@@ -0,0 +1,8 @@
+function makeSandwitch(item1:string,item2:string,item3:string){ 
+    console.log(`your sandwitch's menu include ${item1},${item2},${item3}`)
+    console.log('sandwitch complete')
+
+}
+ makeSandwitch('mayo','onions','eggs')
+ makeSandwitch('ketchup','mushroom','tomato')
+ makeSandwitch('bbq sauce','onions','mushrooms')
