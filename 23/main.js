95033e1257f5d53f4d2495fcefed5737ca21a57b
ezzatsalma <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 16:24:56 +0500

    Add files via upload

---
+++ 23/main.js
@@ -0,0 +1,45 @@
+//1
+var car = "blue";
+console.log("is car == 'blue'? predict true");
+console.log(car == 'blue');
+console.log(car == 'red');
+//2
+var names = "honda";
+console.log("is car !='honda predict false");
+console.log(names == "bolan");
+console.log(names == 'honda');
+//3
+var city = "karachi";
+console.log("is city=='Karachi' predict true ; if not predict false");
+console.log(city == 'cairo');
+console.log(city == 'karachi');
+//4
+var country = 'japan';
+console.log("is country==japan predict true");
+console.log(country == 'china');
+console.log(country == 'japan');
+//5
+var gender = 'girl';
+console.log("is gender = girl predict true");
+console.log(gender == 'girl');
+console.log(gender != 'girl');
+//6
+var color = " blue";
+console.log("if color is not blue predict false");
+console.log(color == 'green');
+//7
+var identity = "salma";
+console.log("if name is salma predict true");
+console.log(identity == "salma");
+//8
+var number = 22;
+console.log("if number is  22 predict true");
+console.log(number == 22);
+//9
+var class_number = 1;
+console.log("if class is 1 predict true");
+console.log(class_number == 1);
+//10
+var person = "female";
+console.log("if female predict true");
+console.log(person == "female");
---
+++ 23/main.ts
@@ -0,0 +1,45 @@
+//1
+let car ="blue";
+console.log("is car == 'blue'? predict true")
+console.log(car == 'blue')
+console.log(car=='red')
+//2
+let names = "honda"
+console.log("is car !='honda predict false")
+console.log(names == "bolan")
+console.log(names=='honda')
+//3
+let city = "karachi"
+console.log("is city=='Karachi' predict true ; if not predict false")
+console.log(city =='cairo')
+console.log(city=='karachi')
+//4
+let country='japan'
+console.log("is country==japan predict true" )
+console.log(country=='china')
+console.log(country=='japan')
+//5
+let gender ='girl'
+console.log("is gender = girl predict true")
+console.log(gender=='girl')
+console.log(gender!='girl')
+//6
+let color = " blue"
+console.log("if color is not blue predict false")
+console.log(color=='green')
+//7
+let identity = "salma"
+console.log("if name is salma predict true")
+console.log(identity=="salma")
+//8
+let number=22
+console.log("if number is  22 predict true")
+console.log(number==22)
+//9
+let class_number = 1
+console.log("if class is 1 predict true")
+console.log(class_number ==1)
+//10
+let person="female"
+console.log("if female predict true")
+console.log(person=="female")
