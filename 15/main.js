cd7c1af83fe6efc1f33db75c6854211ff0e579b5
ezzatsalma <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 16:17:52 +0500

    Add files via upload

---
+++ 15/main.js
@@ -0,0 +1,13 @@
+//let names = ['Ammar', 'salma', 'Hafsa', 'Ammara']
+let message = " 'you are invited to dinner'";
+//console.log(names[0] + message);
+//console.log(names[1] + message)
+//console.log(names[2] + message)
+//console.log(names[3] + message)
+console.log("salma");
+let names = ['AMMAR', 'AMMARA', 'HAFSA'];
+let quantity = names.push('NAILA');
+console.log(names[0] + message);
+console.log(names[1] + message);
+console.log(names[2] + message);
+console.log(names[3] + message);
