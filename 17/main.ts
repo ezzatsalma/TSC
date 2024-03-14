636e385719d9e643ec2276937b5fb8cc2df3b1dd
ezzatsalma <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 16:19:56 +0500

    Add files via upload

---
+++ 17/main.ts
@@ -0,0 +1,23 @@
+let array=['hadia','hania','wania']
+let message="  sorry you're not invited"
+console.log(" BAD NEWS !!!! \n dinner table can't arrive in time; only 2 people can be invited")
+
+
+array.forEach(function(array){
+  
+    console.log( array +   message)
+})
+
+
+
+let abc=['ashir','amir']
+let message1=("  yay! you're invited to dinner")
+abc.forEach(function(abc) {
+    console.log('\n\n'abc +  message1)
+})
+let abd:string[]=[]
+abd.forEach{
+    console.log(abd)
+}
+
+
