73c05fe2431f975af26abcd2673d088896b1af6b
ezzatsalma <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 16:30:27 +0500

    Add files via upload

---
+++ 29/main.js
@@ -0,0 +1,31 @@
+var favourite_fruit = ['apple', 'banana', 'mango'];
+if (favourite_fruit.includes("banana")) {
+    console.log("you really like banana");
+}
+else {
+    console.log("this isn't in your list");
+}
+if (favourite_fruit.includes("orange")) {
+    console.log("you really like orange");
+}
+else {
+    console.log("this isn't in your list");
+}
+if (favourite_fruit.includes("mango")) {
+    console.log("you really like mango");
+}
+else {
+    console.log("this isn't in your list");
+}
+if (favourite_fruit.includes("apple")) {
+    console.log("you really like apple");
+}
+else {
+    console.log("this isn't in your list");
+}
+if (favourite_fruit.includes("guava")) {
+    console.log("you really like guava");
+}
+else {
+    console.log("this isn't in your list");
+}
