830fadb01ecc0c67bba1c8cef0a9ae0f8a88bd86
ezzatsalma <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 16:16:28 +0500

    Add files via upload

---
+++ 13/transport.js
@@ -0,0 +1,5 @@
+"use strict";
+let array = ["rollsroyce", "kia sportage", "tesla"];
+console.log("my dream car is ", array[0]);
+console.log("i will buy ", array[1]);
+console.log("i will also buy", array[2]);
