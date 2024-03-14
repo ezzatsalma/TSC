5344c258d198de7288b6e2edd8c9da4389204823
ezzatsalma <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 16:29:23 +0500

    Add files via upload

---
+++ 27/MAIN.js
@@ -0,0 +1,10 @@
+let eyes = 'green';
+if (eyes == 'green')
+    console.log('5 points');
+else
+    [];
+if (eyes == 'yellow')
+    console.log('10 points');
+();
+if (eyes == 'red')
+    console.log('15 points');
