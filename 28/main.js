a1022833ba5150dba8ff851fec39efe86671e337
ezzatsalma <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 16:29:55 +0500

    Add files via upload

---
+++ 28/main.js
@@ -0,0 +1,13 @@
+var age = 30;
+if (age < 2)
+    console.log("it's a baby");
+else if (age >= 2 && age < 4)
+    console.log("it's a toddler");
+else if (age >= 4 && age < 13)
+    console.log("it's a kid");
+else if (age >= 13 && age < 20)
+    console.log("it's a teenager");
+else if (age >= 20 && age < 65)
+    console.log("it's an adult");
+else if (age == 65)
+    console.log("it's an elder");
