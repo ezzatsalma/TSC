e03ca9c65a4022eae30b2460f2c18d82e17a3927
SALMA EZZAT <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 17:09:32 +0500

    Add files via upload

---
+++ 40/main.js
@@ -0,0 +1,6 @@
+function make_album(artist, album, tracks) {
+    console.log("artist:".concat(artist, " , album:").concat(album, " , tracks").concat(tracks, " "));
+}
+make_album('BTS', 'BE');
+make_album('TAILOR SWIFT', 'LOVER', '10');
+make_album('SUGA', 'D-2', '12');
---
+++ 40/main.ts
@@ -0,0 +1,7 @@
+function make_album(artist:string,album:string,tracks?:string)
+:void{
+    console.log(`artist:${artist} , album:${album} , tracks${tracks} `)
+}
+make_album('BTS','BE' )
+make_album('TAILOR SWIFT','LOVER','10')
+make_album('SUGA','D-2','12')
