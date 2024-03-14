db865e993c6a95a9854cb1533266f97ef5557a54
ezzatsalma <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 16:31:35 +0500

    Add files via upload

--- 30/main.js
+++ 30/main.js
@@ -1,12 +1,20 @@
-var usernames = ['admin', 'eric', 'niel', 'bruce', 'mandy'];
-for (var _i = 0, usernames_1 = usernames; _i < usernames_1.length; _i++) {
-    var username = usernames_1[_i];
-    if (username === 'admin') {
-        console.log('welcome admin! would you like to see status report?');
-    }
-    else {
-        {
-            console.log("welcome ".concat(username));
-        }
-    }
+//let people = ['emad','esma','enna','era']
+var people = [];
+if (people.length == 0) {
+    console.log("we need to add some people");
+}
+else {
+    var people_age = 30;
+    if (people_age < 2)
+        console.log("it's a baby");
+    else if (people_age >= 2 && people_age < 4)
+        console.log("it's a toddler");
+    else if (people_age >= 4 && people_age < 13)
+        console.log("it's a kid");
+    else if (people_age >= 13 && people_age < 20)
+        console.log("it's a teenager");
+    else if (people_age >= 20 && people_age < 65)
+        console.log("it's an adult");
+    else if (people_age == 65)
+        console.log("it's an elder");
 }
--- 30/main.ts
+++ 30/main.ts
@@ -1,12 +1,19 @@
-let usernames=['admin','eric','niel','bruce','mandy']
-for(let username of usernames){
-
-    if(username ==='admin'){
+//let people = ['emad','esma','enna','era']
+let people=[]
+if(people.length==0){
+    console.log("we need to add some people")
     
-    console.log('welcome admin! would you like to see status report?')
-    
-       }   else {
-    {
-    console.log(`welcome ${username}`)
-
-    }}}
+}else{ 
+let people_age=30
+if (people_age<2)
+console.log("it's a baby")
+else if(people_age>=2 && people_age <4)
+console.log("it's a toddler")
+else if(people_age>=4 && people_age <13)
+console.log("it's a kid")
+else if(people_age>=13 && people_age <20)
+console.log("it's a teenager")
+else if(people_age>=20 && people_age <65)
+console.log("it's an adult")
+else if(people_age== 65)
+console.log("it's an elder")}
