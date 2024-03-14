de7a0715538616dc12148e71ce42c97d24a1a539
SALMA EZZAT <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 17:05:10 +0500

    Add files via upload

---
+++ 3,5/main.js
@@ -0,0 +1,7 @@
+var animals = ['dog', 'cat', 'rabbit'];
+for (var _i = 0, animals_1 = animals; _i < animals_1.length; _i++) {
+    var animal = animals_1[_i];
+    console.log("it' a/an ".concat(animal));
+    console.log("".concat(animal, "would make a great pet"));
+}
+console.log('any of these animals would make a great pet');
---
+++ 3,5/main.ts
@@ -0,0 +1,8 @@
+let animals=['dog','cat','rabbit']
+for(let animal of animals)
+{
+    console.log(`it' a/an ${animal}`)
+
+console.log(`${animal} would make a great pet`)
+}
+console.log('any of these animals would make a great pet')
