397c9596cb86d01af3261884711d80a0deb3491a
SALMA EZZAT <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 17:15:40 +0500

    Delete assignments directory

--- assignments/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-console.log("hello world");
-let pname = "Ely";
-console.log("hello", pname);
-console.log("how are you", pname);
--- assignments/main.ts
+++
@@ -1,4 +0,0 @@
-console.log("hello world");
-let pname="Ely";
-console.log("hello"pname);
-console.log("how are you"pname);
