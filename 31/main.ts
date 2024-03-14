4682447f31e1d36731b76099469f1add78c6f397
ezzatsalma <163161326+ezzatsalma@users.noreply.github.com>
2024-03-14 16:32:18 +0500

    Add files via upload

---
+++ 31/main.ts
@@ -0,0 +1,12 @@
+let usernames=['admin','eric','niel','bruce','mandy']
+for(let username of usernames){
+
+    if(username ==='admin'){
+    
+    console.log('welcome admin! would you like to see status report?')
+    
+       }   else {
+    {
+    console.log(`welcome ${username}`)
+
+    }}}
